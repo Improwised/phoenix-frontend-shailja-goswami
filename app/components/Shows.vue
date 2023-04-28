a35bdd1232e6388cdd645c0b29a0add60538702c
Shailja <shailja.goswami@improwised.com>
2023-04-28 16:35:02 +0530

    get api call and designed the page

--- app/components/Shows.vue
+++ app/components/Shows.vue
@@ -1,14 +1,42 @@
+<script setup>
+const props = defineProps(['showsData'])
+const shows = ref(props.showsData.data)
+// const isPending = ref(props.ShowsData.isPending)
+</script>
+
 <template>
   <div>
-    <div class="card card-show-brief">
-      <img src="..." class="card-img-top" alt="..." />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" class="btn btn-theme-blue">Go somewhere</a>
+    <div v-if="!isPending" class="row">
+      <div
+        v-for="(show, index) in shows"
+        :key="index"
+        class="col-md-4 col-lg-3 col-sm-10 g-4"
+      >
+        <div class="card card-show-brief">
+          <img
+            :src="show.img_url"
+            class="card-img-top img-show"
+            :alt="show.title"
+          />
+          <div class="card-body">
+            <h5 class="card-title text-truncate">{{ show.title }}</h5>
+            <div class="card-text">
+              <div class="show-desc text-gray mt-2">
+                {{ show.desc }}
+              </div>
+              <div class="d-flex justify-content-between mt-3">
+                <div class="text-truncate text-gray">
+                  Played: {{ show.runtime }}
+                </div>
+                <div class="text-truncate text-gray">{{ show.type }}</div>
+                <div class="text-truncate text-gray">
+                  {{ show.release_year }}
+                </div>
+              </div>
+            </div>
+            <a href="#" class="btn btn-theme-blue">Go somewhere</a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
--- app/pages/discover/index.vue
+++ app/pages/discover/index.vue
@@ -1,11 +1,26 @@
+<script setup>
+const config = useRuntimeConfig()
+const baseUrl = config.public.base_url
+console.log(baseUrl)
+const { data: showsData, pending } = await useLazyFetch(`${baseUrl}/title`)
+const searchString = ref('')
+let shows = {
+  data: showsData,
+  isPending: pending,
+}
+</script>
+
 <template>
-  <section class="container pt-5">
+  <section class="container pt-md-5">
+    <div class="col-4 offset-8">
+      <input
+        v-model="searchString"
+        class="form-control"
+        placeholder="Search for Movies and Tv show"
+      />
+    </div>
     <div class="col-12">
-      <div class="row">
-        <div class="col-md-4 col-lg-3 col-sm-10 g-4">
-          <Shows></Shows>
-        </div>
-      </div>
+      <Shows :shows-data="shows"></Shows>
     </div>
   </section>
 </template>
