cc415b9528b4229565583906c3a35d90cbfef5a6
Shailja <shailja.goswami@improwised.com>
2023-05-04 18:54:28 +0530

    bootstrap modal added

--- app/components/Shows.vue
+++ app/components/Shows.vue
@@ -4,16 +4,21 @@ const config = useRuntimeConfig()
 const base_url = config.public.base_url
 const shows = ref(props.showsData.data)
 const isPending = ref(props.showsData.isPending)
-
-async function deleteShow(id, show) {
-  if (confirm('Are you sure you wanted to remove ' + show + ' this show')) {
-    const deleted = await useFetch(`${base_url}/title/${id}`, {
-      method: 'DELETE',
-    })
-    window.location.href = '/discover'
-  } else {
-    return
-  }
+const showModal = ref(false)
+let id
+function openModal(showId) {
+  showModal.value = true
+  id = showId
+}
+function closeModal() {
+  showModal.value = false
+  refreshNuxtData()
+}
+async function deleteShow() {
+  const deleted = await useFetch(`${base_url}/title/${id}`, {
+    method: 'DELETE',
+  })
+  window.location.href = '/discover?_page=1'
 }
 </script>
 
@@ -61,8 +66,8 @@ async function deleteShow(id, show) {
                   src="~/assets/images/icons/delete.svg"
                   class="main__table-btn btn_delete cursor-pointer"
                   data-bs-toggle="modal"
-                  data-bs-target="#exampleModal"
-                  @click="deleteShow(show.id, show.title)"
+                  data-bs-target="#deleteModal"
+                  @click="openModal(show.id)"
                 />
               </div>
             </div>
@@ -71,4 +76,58 @@ async function deleteShow(id, show) {
       </div>
     </div>
   </div>
+  <div
+    id="deleteModal"
+    class="modal fade"
+    tabindex="-1"
+    aria-labelledby="deleteModalLabel"
+    :aria-hidden="showModal ? true : false"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content modal-dark">
+        <div class="modal-header">
+          <h5 id="deleteModalLabel" class="modal-title">Delete Show</h5>
+        </div>
+        <div class="modal-body">Are you sure You want to remove the show ?</div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+            @click="closeModal()"
+          >
+            Close
+          </button>
+          <button
+            type="button"
+            class="btn btn-theme-blue"
+            @click="deleteShow()"
+          >
+            Save changes
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<style>
+.modal-dark {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  color: white;
+  pointer-events: auto;
+  background-color: #192842;
+  background-clip: padding-box;
+  outline: 0;
+}
+
+.modal-header {
+  border-bottom: #151f30;
+}
+
+.modal-footer {
+  border: none;
+}
+</style>
